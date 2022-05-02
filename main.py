54fddf702e8bdee0ee8f37f23c3a8de7c9f30214
Reman Sunuwar <97722370+remansunuwarofficial@users.noreply.github.com>
2022-05-02 19:10:05 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+#problem 1
+'''
+Own dictionary
+Create a dictionary and take input from the user and return the meaning of the
+from your dictionary
+'''
+dictionary = {
+    "url": "Universal Resource Locator",
+    "attest": "provide proof of",
+    "audio": "sound",
+    "yak": "an animal"
+}
+usersKey = input("Enter a Word ")
 
-#faulty Calculator
+print(dictionary[usersKey])
+
+
+#problem 2
+'''
+faulty Calculator
+Design a calculator which will correctly solve all the problems except following problems
+(45 * 3 = 555, 56  + 9 = 77, 56 / 6 = 4)
+'''
 firstNum = int(input("Enter First Number "))
 operation = input("Enter Which Operation do you want to perform ")
 secondNum = int(input("Enter Second Number "))
